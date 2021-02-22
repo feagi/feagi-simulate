871fb09efb90d9cb51853abafdf9a4bc7ff0effa
Kakcalu13 <Kevin.A.Araujo@gmail.com>
2021-02-22 08:03:14 -0700

    update to see if it works now

--- keyboardtest.py
+++ keyboardtest.py
@@ -113,11 +113,11 @@ class AtlasTeleop(object):
         try:
             self.init()
             self.print_usage()
-	    print "get key = " + (self.get_key())
+	    #print "get key = " + (self.get_key())
             while not (self.get_key()) == 'q': #this is the infinite loop 
                 ch = self.get_key()
                 self.process_key(ch)
-                print "the answer is " + ch
+                print "the answer is " + ch #for debug to identify
         finally:
             self.fini()
 
