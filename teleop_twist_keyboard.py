bccbbad9f5d10dee7dbf22f39da19a8b52f2f0b2
Kakcalu13 <Kevin.A.Araujo@gmail.com>
2021-03-03 06:43:15 -0700

    update the obstacle detection

--- teleop_twist_keyboard.py
+++ teleop_twist_keyboard.py
@@ -134,7 +134,7 @@ if __name__=="__main__":
     try:
         print(msg)
         while(1):
-            print(format(dt.ranges[0]))
+            print(dt.callback)
             key = getKey()
             if key == 'w' :
                 target_linear_vel = target_linear_vel + 2
